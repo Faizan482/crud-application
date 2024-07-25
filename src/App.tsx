8735a8204771dcd54430aa920c564f3d42ee9b84
muhammad-Faizan482 <faizanlatif481@gmail.com>
2024-07-25 17:18:14 +0500

    add mui icons and import addTodoForm and TodoList in app.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Container } from "@mui/material";
 import NavBar from "./components/NavBar";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 const App = () => {
   return (
     <>
       <NavBar />
+      <Container maxWidth="xl">
+        <AddTodoForm />
+        <TodoList />
+      </Container>
     </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import store, { persistor } from "./store";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
+);
